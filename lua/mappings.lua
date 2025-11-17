cb2e87958198b9286d98289e7ad46c602edd7a41
zhengyanglee07 <zhengyangz1007@gmail.com>
2025-11-17 16:54:17 +0800

    feat: spectre plugins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -56,3 +56,9 @@ map("n", "<leader>fD", "<cmd>FlutterDevTools<CR>", { desc = "Flutter DevTools" }
 map("n", "<leader>fl", "<cmd>FlutterLspRestart<CR>", { desc = "Flutter LSP Restart" })
 map("n", "<leader>fo", "<cmd>FlutterOutlineToggle<CR>", { desc = "Flutter Outline" })
 map("n", "<leader>fp", "<cmd>FlutterPubGet<CR>", { desc = "Flutter Pub Get" })
+
+-- Spectre (find and replace) keymaps
+map("n", "<leader>S", "<cmd>lua require('spectre').toggle()<CR>", { desc = "Toggle Spectre" })
+map("n", "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", { desc = "Search current word" })
+map("v", "<leader>sw", "<esc><cmd>lua require('spectre').open_visual()<CR>", { desc = "Search current word" })
+map("n", "<leader>sp", "<cmd>lua require('spectre').open_file_search({select_word=true})<CR>", { desc = "Search in current file" })
